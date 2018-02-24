e9cc7c9bf714c68a5071fe2bad0140039cdb692d
evandersar <evandersar@ukr.net>
2018-02-24 15:51:14 +0200

    basic design for create and details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NewsListComponent } from './news-list/news-list.component';
 import { NewsService } from './news.service';
 import { PaginationStateService } from './pagination-state.service';
+import { CreateNewsComponent } from './create-news/create-news.component';
+import { NewsDetailsComponent } from './news-details/news-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsListComponent
+    NewsListComponent,
+    CreateNewsComponent,
+    NewsDetailsComponent
   ],
   imports: [
     BrowserModule
